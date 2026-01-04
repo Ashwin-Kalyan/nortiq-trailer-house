9e4c40076a05b25ede5ff147b5be4b3e4338e089
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2026-01-04 10:05:58 -0800

    vercel

--- src/pages/OperationPage.tsx
+++ src/pages/OperationPage.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
 
 const OperationPage = () => {
   const [activeFaq, setActiveFaq] = useState<number | null>(null)
